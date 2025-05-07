6dfbdfa3b3490ebd3578e1ab086123bfbfd669dc
TheWhiteSunexe <Gillet.tristan.94@gmail.com>
2025-05-07 15:31:17 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  s3: {
+    accessKeyId: 'VUVGHISOY9L8DIQURTLM', // TODO
+    secretAccessKey: 'XVK1OcWLycfQAo18OnbD6vsEGcaeGeZW2P7zVAMK', // TODO
+    endpoint: 's3.eu-west-2.wasabisys.com', // TODO
+    bucket: 'esgi-cloud-exam-bucket-91069', // TODO
+  },
+  suffix: {
+    small: '_small.jpg',
+    full: '_full.jpg'
+  }
+}
